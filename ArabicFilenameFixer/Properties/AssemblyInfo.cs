cda72123e8052f7e7c654d6d9fcd80429a049bee
HirbodBehnam <chrome.hiri.angry@gmail.com>
2022-05-12 15:48:30 +0430

    Added licence and stuff

--- ArabicFilenameFixer/Properties/AssemblyInfo.cs
+++ ArabicFilenameFixer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ArabicFilenameFixer")]
+[assembly: AssemblyTitle("Arabic Filename Fixer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ArabicFilenameFixer")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("Arabic Filename Fixer")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
